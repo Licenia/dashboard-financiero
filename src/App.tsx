c7ff9a2c8a6b92a62bc43962f846bfc008174644
Licenia Alvear <licialvear@gmail.com>
2026-08-20 09:45:05 -0500

    feat(app): agregar calculos dinamicos de saldo, ingresos y gastos al resumen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import Movimientos from "./components/Movimientos"
-import Resumen from "./components/Resumen"
+import Movimientos from "./components/Movimientos";
+import Resumen from "./components/Resumen";
 
 function App() {
-
   const movimientos = [
     {
-       id: 1,
-       monto: 800,
-       descripcion: "Transferencia"
-     },
+      id: 1,
+      monto: 800,
+      descripcion: "Transferencia",
+      tipo: "ingreso",
+    },
     {
-       id: 2,
-       monto: 500,
-       descripcion: "Comida"
-     },
+      id: 2,
+      monto: 500,
+      descripcion: "Comida",
+      tipo: "gasto",
+    },
     {
-       id: 3,
-       monto: 80,
-       descripcion: "Transporte"
-     }
-  ]
+      id: 3,
+      monto: 80,
+      descripcion: "Transporte",
+      tipo: "gasto",
+    },
+  ];
+
+  const gastos = movimientos
+    .filter((movimiento) => movimiento.tipo === "gasto")
+    .reduce((acumulador, gasto) => acumulador + gasto.monto, 0);
+
+  const ingresos = movimientos
+    .filter((movimientos) => movimientos.tipo === "ingreso")
+    .reduce((acumulador, ingreso) => acumulador + ingreso.monto, 0);
 
   return (
     <>
@@ -31,25 +41,33 @@ function App() {
       </nav>
 
       <section className="container">
-      <div>
-        <ul className="list">
-          <li>Dashboard</li>
-          <li>Movimientos</li>
-          <li>Transferir</li>
-          <li>Configuracion</li>
-        </ul>
+        <div>
+          <ul className="list">
+            <li>Dashboard</li>
+            <li>Movimientos</li>
+            <li>Transferir</li>
+            <li>Configuracion</li>
+          </ul>
         </div>
         <div>
           <h2>Buenos dias, Licenia</h2>
-            <Resumen/>
-           <h2> Ultimos movimientos</h2>
+          <Resumen
+            saldo={ingresos - gastos}
+            ingresos={ingresos}
+            gastos={gastos}
+          />
+          <h2> Ultimos movimientos</h2>
           {movimientos.map((movimiento) => (
-            <Movimientos key={movimiento.id} monto={movimiento.monto} descripcion={movimiento.descripcion} />
-         ))}
+            <Movimientos
+              key={movimiento.id}
+              monto={movimiento.monto}
+              descripcion={movimiento.descripcion}
+            />
+          ))}
         </div>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
