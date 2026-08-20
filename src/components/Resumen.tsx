93b37c4259b92546c3ede28b3fb17b103c5e46ff
Licenia Alvear <licialvear@gmail.com>
2026-08-20 09:42:27 -0500

    feat(resumen): agregar props al componente

--- src/components/Resumen.tsx
+++ src/components/Resumen.tsx
@@ -1,18 +1,18 @@
 
-function Resumen() {
+function Resumen(props) {
   return (
     <div className="tarjeta-resumen">
       <div>
       <h3>Saldo disponible</h3>
-      <p>$555.00</p>
+      <p>{props.saldo}</p>
       </div>
       <div>
         <h3>Ingresos</h3>
-        <p>$1,800.00</p>
+        <p>{props.ingresos}</p>
       </div>
       <div>
       <h3>Gastos</h3>
-      <p>$1,245.00</p>
+      <p>{props.gastos}</p>
       </div>
     </div>
   )
