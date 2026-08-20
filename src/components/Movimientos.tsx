70ebce651dce6243d3d7d2ce22e84e438883274f
Licenia Alvear <licialvear@gmail.com>
2026-08-20 09:56:12 -0500

    feat(Resumen, Movimientos): agrega tipado a las props

--- src/components/Movimientos.tsx
+++ src/components/Movimientos.tsx
@@ -1,11 +1,18 @@
-function Movimientos(props) {
+type ResumenProps = {
+  monto: number;
+  descripcion: string;
+};
+
+function Movimientos(props: ResumenProps) {
   return (
     <div>
       <ul className="list">
-        <li>{props.monto} {props.descripcion}</li>
+        <li>
+          {props.monto} {props.descripcion}
+        </li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default Movimientos
+export default Movimientos;
--- src/components/Resumen.tsx
+++ src/components/Resumen.tsx
@@ -1,21 +1,26 @@
+type ResumenProps = {
+  saldo: number;
+  ingresos: number;
+  gastos: number;
+};
 
-function Resumen(props) {
+function Resumen(props: ResumenProps) {
   return (
     <div className="tarjeta-resumen">
       <div>
-      <h3>Saldo disponible</h3>
-      <p>{props.saldo}</p>
+        <h3>Saldo disponible</h3>
+        <p>{props.saldo}</p>
       </div>
       <div>
         <h3>Ingresos</h3>
         <p>{props.ingresos}</p>
       </div>
       <div>
-      <h3>Gastos</h3>
-      <p>{props.gastos}</p>
+        <h3>Gastos</h3>
+        <p>{props.gastos}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Resumen
+export default Resumen;
